392a71fcec48f6a5bb539bcc0b0583e85608ae52
venkatvignes <venkatvjn007@gmail.com>
2023-04-29 17:03:48 +0530

    change index

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const serverless = require('serverless-http')
+
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const uri = process.env.ATLAS_URI;
+
+const app = express()
+
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.use(express.json());
+
+
+mongoose.connect(uri)
+        .then(() => {
+          console.log("hi mongo");
+        })
+
+  const exerciseroutes = require('./routes/exercise')
+  const userroutes = require('./routes/user')
+
+  app.use('/functions/exercises',exerciseroutes)
+  app.use('/functions/users', userroutes)
+
+app.listen(port,()=>{
+    console.log(`server is running port: ${port}`)
+})
+
+module.exports.handler = serverless(app)
